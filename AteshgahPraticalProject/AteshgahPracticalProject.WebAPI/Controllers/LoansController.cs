c96314cbbb7f31282fcc0a66231b2443c85303f4
ilkinbayram <ilkinbayramsoy@gmail.com>
2020-12-20 16:42:20 +0400

    Finished Services

--- AteshgahPraticalProject/AteshgahPracticalProject.WebAPI/Controllers/LoansController.cs
+++ AteshgahPraticalProject/AteshgahPracticalProject.WebAPI/Controllers/LoansController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AteshgahPracticalProject.Business.Abstract;
+using AteshgahPracticalProject.Core.Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +11,44 @@ namespace AteshgahPracticalProject.WebAPI.Controllers
 {
     public class LoansController : ApiController
     {
+        private readonly ILoanService _loanService;
+
+
+        public LoansController(ILoanService loanService)
+        {
+            _loanService = loanService;
+        }
+
+        public HttpResponseMessage GetLoan(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, _loanService.Get(id));
+        }
+
+        public HttpResponseMessage GetLoans()
+        {
+
+            return Request.CreateResponse(HttpStatusCode.OK, _loanService.GetAll());
+        }
+
+        public HttpResponseMessage PostLoan(Loan loan)
+        {
+            try
+            {
+                if (_loanService.Add(loan) != null)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, loan);
+                    response.Headers.Location = new Uri(Request.RequestUri + "/" + loan.ID);
+                    return response;
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Insert Process Failed.");
+                }
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest,"Please Contact with the admin. Process Did Not Successed");
+            }
+        }
     }
 }
