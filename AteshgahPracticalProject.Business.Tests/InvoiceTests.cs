179417f9c1fa31a39a01f844b93807a954cc0dea
ilkinbayram <ilkinbayramsoy@gmail.com>
2020-12-20 17:52:27 +0400

    Last Changes Added

---
+++ AteshgahPracticalProject.Business.Tests/InvoiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AteshgahPracticalProject.Business.Abstract;
+using AteshgahPracticalProject.Business.DependencyResolvers.Ninject;
+using AteshgahPracticalProject.DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AteshgahPracticalProject.Business.Tests
+{
+    [TestClass]
+    public class InvoiceTests
+    {
+        [TestInitialize]
+        public void Invoice_Test_Initialize()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void Check_SP_Loan_Amount_Counter_Is_Right()
+        {
+            EfInvoiceDal invoiceDal = new EfInvoiceDal();
+
+            // Calculated Before
+            decimal rightAmount = 105;
+
+            decimal result = invoiceDal.SP_CalculateInvoiceAmount(3, 300, 3, 5);
+
+            Assert.IsTrue(rightAmount == result);
+        }
+    }
+}
